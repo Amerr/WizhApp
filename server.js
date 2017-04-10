63d8ceb9d2d63f0df4e2cc3cb7a31560e4056db1
Dineshkarthik Raveendran <dineshkarthik@theviralfever.com>
2017-04-10 16:58:36 +0530

    ENH: Adding fluent-ffmpeg

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ var express = require('express'),
     formidable = require('formidable'),
     fs = require('fs'),
     path = require('path'),
-    exec = require('child_process').exec;
+    exec = require('child_process').exec,
+    ffmpeg = require('fluent-ffmpeg');
 
-var app = express();
-var uploadPath = path.join(__dirname, '/uploads');
+var app = express(),
+    uploadPath = path.join(__dirname, '/uploads');
+
 var server = app.listen(8000, function() {
     console.log('Server listening on port 8000');
 });
@@ -21,7 +23,7 @@ app.get('/', function(req, res) {
 
 
 app.get('/compile', function(req, res) {
-    var user = req.params('user');
+    var user = req.query.user;
     var dir = path.join(__dirname, '/output/' + user);
     var inDir = path.join(__dirname, '/uploads/' + user);
     var beat = path.join(__dirname, '/audio/upbeat.mp3');
@@ -43,6 +45,37 @@ app.get('/compile', function(req, res) {
                     'success': true
                 });
             }
+        });
+    });
+});
+
+app.get('/render', function(req, res) {
+    var user = req.query.user,
+        dir = path.join(__dirname, '/output/' + user),
+        inDir = path.join(__dirname, '/uploads/' + user),
+        beat = path.join(__dirname, '/audio/upbeat.mp3'),
+        command = ffmpeg();
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    fs.readdir(inDir, function(err, list) {
+        fileFullPath(inDir, list).forEach(function(videoName){
+            command = command.addInput(videoName);
+        });
+        command.mergeToFile(dir + '/fluent_output.mp4')
+        .on('error', function(err) {
+            console.log('Error ' + err.message);
+            res.status(500);
+            res.json({
+                'success': false
+            });
+        })
+        .on('end', function() {
+            console.log('Finished!');
+            res.status(200);
+            res.json({
+                'success': true
+            });
         });
     });
 });
@@ -73,7 +106,7 @@ function concatOperator(list, input) {
 }
 
 function conctatOutput(input, dir) {
-    return input + ' -strict -2 -y ' + dir + '/output.mp4'
+    return input + ' -strict -2 -y ' + dir + '/cmd_output.mp4'
 }
 
 app.post('/upload', function(req, res) {
